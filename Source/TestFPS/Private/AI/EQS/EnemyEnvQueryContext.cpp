8c33a7732c51cc7801c086becd040abb551bfd51
rayman30011 <rayman30011@gmail.com>
2021-04-22 13:47:47 +0600

    added FindEnemy EQS context

---
+++ Source/TestFPS/Private/AI/EQS/EnemyEnvQueryContext.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/EQS/EnemyEnvQueryContext.h"
+
+
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+
+void UEnemyEnvQueryContext::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
+{
+    const auto QueryOwner = Cast<AActor>(QueryInstance.Owner.Get());
+    const auto Blackboard = UAIBlueprintHelperLibrary::GetBlackboard(QueryOwner);
+    if (!Blackboard) return;
+
+    const auto ContextActor = Blackboard->GetValueAsObject(EnemyActorKeyName);
+    UEnvQueryItemType_Actor::SetContextHelper(ContextData, Cast<AActor>(ContextActor));
+}
